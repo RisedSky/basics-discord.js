32070e7794314ad2f489ba655d5e5d67510abc73
Tanya575 <80748971+Tanya575@users.noreply.github.com>
2021-04-28 14:27:19 +0400

    main

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const prefix = 'put here your bot prefix';
 
 client.once('ready' , () => {
 console.log('online')
-});
+})
 
 client.on('message' , message => {
 if(!message.content.startWith(prefix) || message.author.bot) return; // this mean if the command does not start with prefix or it was from a bot he will return
