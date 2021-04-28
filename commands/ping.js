9a85223f9208dda2fb6a34f5d5630f3d89fec941
Tanya575 <80748971+Tanya575@users.noreply.github.com>
2021-04-28 13:59:19 +0400

    commit 3

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,7 @@
 module.exports = {
     name: 'ping',
-    aliases: ['p', 'بنق'],
-    permissions: ["ADMINISTRATOR"],
     description: "this is a ping command!",
     execute(client, message, args){
-        message.channel.send('pong!');
+        message.channel.send('pong');
     }
 }
